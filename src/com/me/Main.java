c67bb4b924282c9bdebb49a5414200a01b5167fe
patrickatcw <pj8392@yahoo.com>
2018-03-04 10:52:57 -0500

    organizing same line with commas

--- src/com/me/Main.java
+++ src/com/me/Main.java
@@ -8,11 +8,11 @@ public class Main {
 
         //defining an array with the number of ints
         int[] myIntArray = new int[3];  //3 ints in array, although none are assigned values
-        myIntArray[0] = 10;         //defining values of individual ints
-        myIntArray[1] = 20;
-        myIntArray[2] = 30;
-        System.out.println(myIntArray[0]);
-        System.out.print(myIntArray[1]);    //notice no "ln", puts 1 and 2 on same line, second line
+        myIntArray[0] = 100;         //defining values of individual ints
+        myIntArray[1] = 200;
+        myIntArray[2] = 300;
+        System.out.print(myIntArray[0] + ", ");
+        System.out.print(myIntArray[1] + ", ");
         System.out.println(myIntArray[2]);
 
     }
